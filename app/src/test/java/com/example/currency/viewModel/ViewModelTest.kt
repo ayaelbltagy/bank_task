d596b01732a4dac792b752ffe721dd4745cba0c1
ayaelbltagy <a.elbeltagy@etmana.com>
2023-07-30 12:13:04 +0300

    test rates api response

--- app/src/test/java/com/example/currency/viewModel/ViewModelTest.kt
+++ app/src/test/java/com/example/currency/viewModel/ViewModelTest.kt
@@ -2,9 +2,12 @@ package com.example.currency.viewModel
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.example.currency.data.data.models.AllCurrenciesResponse
+import com.example.currency.data.data.models.LatestRateResponse
 import com.example.currency.data.data.repository.MainRepository
 import com.example.currency.data.data.viewModel.MainViewModel
 import com.example.currency.helpers.NetworkHelper
+import com.example.currency.helpers.Status
+import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.mockk
 import kotlinx.coroutines.*
@@ -18,14 +21,11 @@ class ViewModelTest {
 
     @Mock
     private var mainRepository = mockk<MainRepository>(relaxed = true)
-
     @Mock
     private val networkHelper = mockk<NetworkHelper>(relaxed = true)
     private lateinit var viewModel: MainViewModel
 
 
-    @get:Rule
-    val rule: TestRule = InstantTaskExecutorRule()
 
     @Before
     fun setup() {
@@ -39,6 +39,7 @@ class ViewModelTest {
 
     }
 
+
     @Test
     fun `should emit success when network is connected`() {
         if (networkHelper.isNetworkConnected()) {
@@ -50,7 +51,32 @@ class ViewModelTest {
 
         }
     }
+    @Test
+    fun `should emit null  in currencies when network is not connected`() {
+        if (networkHelper.isNetworkConnected()) {
+            lateinit var response: AllCurrenciesResponse
+            coEvery {
+                response = viewModel.currencies().value!!.data!!
+            }
+            Assert.assertEquals(null, response)
 
+        }
+    }
+
+    @Test
+    fun `should emit null in rates when network is not connected`() {
+        if (networkHelper.isNetworkConnected()) {
+              var response= viewModel.latestRates("USD").value!!.status!!
+                Assert.assertEquals(Status.SUCCESS, response)
+        }
+    }
+@Test
+    fun `should emit value in rates when network is connected`() {
+        if (networkHelper.isNetworkConnected()) {
+             var response = viewModel.latestRates("USD").value!!.data!!
+                Assert.assertEquals("2023-07-30", response.date)
+        }
+    }
 
 }
 
